cd22d05bb66c96953e600e2b0e185c60e81735d7
Walrus <81704984+nagaming-master@users.noreply.github.com>
2023-03-26 13:36:37 -0400

    Final git commit for pre-pre-alpha

--- config.lua
+++ config.lua
@@ -1,78 +1,11 @@
 Config = Config or {}
 Config.UseTarget = GetConvar('UseTarget', 'false') == 'true' -- Use qb-target interactions (don't change this, go to your server.cfg and add `setr UseTarget true` to use this and just that from true to false or the other way around)
-Config.CopsChance = 0.5 -- The chance of the cops getting called when a coral gets picked up, this ranges from 0.0 to 1.0
+--Config.CopsChance = 0.1 -- The chance of the cops getting called when a coral gets picked up, this ranges from 0.0 to 1.0
+Config.oxygenlevel = 200 -- this is oxygen level you can change this numper as you like 
 Config.CoralLocations = {
+    -- CHUMASH / ZANCUDO SOUTH --
     [1] = {
-        label = "This is Location 1",
-        coords = {
-            Area = vector3(-2838.8, -376.1, 3.55),
-            Coral = {
-                [1] = {
-                    coords = vector3(-2849.25, -377.58, -40.23),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-                [2] = {
-                    coords = vector3(-2838.43, -363.63, -39.45),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-                [3] = {
-                    coords = vector3(-2887.04, -394.87, -40.91),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-                [4] = {
-                    coords = vector3(-2808.99, -385.56, -39.32),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-            }
-        },
-        DefaultCoral = 4,
-        TotalCoral = 4,
-    },
-    [2] = {
-        label = "Location 2",
-        coords = {
-            Area = vector3(-3288.2, -67.58, 2.79),
-            Coral = {
-                [1] = {
-                    coords = vector3(-3275.03, -38.58, -19.21),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-                [2] = {
-                    coords = vector3(-3273.73, -76.0, -26.81),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-                [3] = {
-                    coords = vector3(-3346.53, -50.4, -35.84),
-                    length = 3,
-                    width = 3,
-                    heading = 100.0,
-                    PickedUp = false
-                },
-            }
-        },
-        DefaultCoral = 3,
-        TotalCoral = 3,
-    },
-    [3] = {
-        label = "Location 3",
+        label = "North of Chumash",
         coords = {
             Area = vector3(-3367.24, 1617.89, 1.39),
             Coral = {
@@ -102,27 +35,141 @@ Config.CoralLocations = {
         DefaultCoral = 3,
         TotalCoral = 3,
     },
-    [4] = {
-        label = "Location 4",
+    -- ZANCUDO --
+    [2] = {
+        label = "West of Zancudo",
         coords = {
-            Area = vector3(3002.5, -1538.28, -27.36),
+            Area = vector3(-2751.72, 3894.83, -27.28),
             Coral = {
                 [1] = {
-                    coords = vector3(2978.05, -1509.07, -24.96),
+                    coords = vector3(-2785.88, 3893.94, -25.84),
                     length = 3,
                     width = 3,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [2] = {
-                    coords = vector3(3004.42, -1576.95, -29.36),
+                    coords = vector3(-2766.06, 3915.71, -27.28),
                     length = 3,
                     width = 3,
                     heading = 100.0,
                     PickedUp = false
                 },
                 [3] = {
-                    coords = vector3(2951.65, -1560.69, -28.36),
+                    coords = vector3(-2751.72, 3894.83, -27.28),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [4] = {
+                    coords = vector3(-2784.02, 3884.84, -24.62),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [5] = {
+                    coords = vector3(-2804.43, 3898.49, -24.62),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+            },
+        },
+        DefaultCoral = 5,
+        TotalCoral = 5,
+    },
+    -- LIGHTHOUSE ISLANDS --
+    [3] = {
+        label = "Lighthouse Islands",
+        coords = {
+            Area = vector3(3627.61, 5337.4, -37.27),
+            Coral = {
+                [1] = {
+                    coords = vector3(3627.61, 5337.4, -37.27),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [2] = {
+                    coords = vector3(3650.65, 5332.68, -37.02),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [3] = {
+                    coords = vector3(3635.84, 5368.28, -34.56),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [4] = {
+                    coords = vector3(3595.02, 5346.67, -36.13),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [5] = {
+                    coords = vector3(3621.83, 5296.59, -33.8),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+            },
+        },
+        DefaultCoral = 5,
+        TotalCoral = 5,
+    },
+    -- EAST OF SAHP HQ --
+    [4] = {
+        label = "East of SAHP HQ",
+        coords = {
+            Area = vector3(3021.48, -74.25, -22.59),
+            Coral = {
+                [1] = {
+                    coords = vector3(3021.48, -74.25, -22.59),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [2] = {
+                    coords = vector3(2993.97, -81.88, -18.43),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [3] = {
+                    coords = vector3(3003.86, -54.79, -21.52),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [4] = {
+                    coords = vector3(3051.7, -77.13, -22.97),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [5] = {
+                    coords = vector3(3037.45, -94.98, -26.75),
+                    length = 3,
+                    width = 3,
+                    heading = 100.0,
+                    PickedUp = false
+                },
+                [6] = {
+                    coords = vector3(3031.69, -44.55, -25.74),
                     length = 3,
                     width = 3,
                     heading = 100.0,
@@ -130,11 +177,12 @@ Config.CoralLocations = {
                 },
             }
         },
-        DefaultCoral = 3,
-        TotalCoral = 3,
+        DefaultCoral = 6,
+        TotalCoral = 6,
     },
+    -- EAST OF PIRATE COVE --
     [5] = {
-        label = "Location 5",
+        label = "Pirate Cove",
         coords = {
             Area = vector3(3421.58, 1975.68, 0.86),
             Coral = {
@@ -171,8 +219,9 @@ Config.CoralLocations = {
         DefaultCoral = 4,
         TotalCoral = 4,
     },
-    [6] = {
-        label = "Location 6",
+    -- PALOMINO BAY --
+    /*[6] = {
+        label = "Palomino Bay",
         coords = {
             Area = vector3(2720.14, -2136.28, 0.74),
             Coral = {
@@ -208,9 +257,10 @@ Config.CoralLocations = {
         },
         DefaultCoral = 4,
         TotalCoral = 4,
-    },
-    [7] = {
-        label = "Location 7",
+    },*/
+    -- NORTHERN ISLANDS --
+    [6] = {
+        label = "Northern Islands",
         coords = {
             Area = vector3(536.69, 7253.75, 1.69),
             Coral = {
@@ -282,11 +332,11 @@ Config.PriceModifiers = {
 }
 Config.SellLocations = {
     [1] = {
-        coords = vector4(-1684.13, -1068.91, 13.15, 100.0),
-        model = 'a_m_m_salton_01',
-        zoneOptions = { -- Only used when not using the target
-            length = 3,
-            width = 3
-        }
+        coords = vector4(-2174.79, 4294.64, 49.05, 198.2),
+        model = 'a_m_m_salton_01'
+    },
+    [2] = {
+        coords = vector4(3801.36, 4478.04, 5.99, 164.75),
+        model = 'a_m_m_salton_01'
     }
 }
